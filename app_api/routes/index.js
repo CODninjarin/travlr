b437870101df2fad7c31bb6136795e4806ab6b70
Brandon Randall <codninjarin72797@gmail.com>
2024-06-09 06:55:29 -0500

    Module 5 Completed Baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express'); // Express app
+const router = express.Router(); // Router logic
+
+// This is where we import the controllers we will route
+const tripsController = require('../controllers/trips');
+
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList); // GET Method routes tripList
+
+module.exports = router;
+
+// GET Method routes tripFindByCode
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode); 
+module.exports = router;
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const ctrlMain = require('../controllers/travel');
+const controller = require('../controllers/travel');
 
-/* GET home page. */
-router.get('/',ctrlMain.travel);
+/* GET travel page. */
+router.get('/',controller.travel);
 
 module.exports = router;
